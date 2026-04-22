5c047b99e2383420114c94176ac7a76c8744346f
gabiii12 <alfonzogab12@gmail.com>
2026-04-23 01:50:43 +0800

    Updated design for mobile version

--- Calculator-project/src/Header.jsx
+++ Calculator-project/src/Header.jsx
@@ -1,28 +1,43 @@
+import { useState } from "react";
 import "./header.css";
 
-function Header({isDark, setIsDark}) {
+function Header({ isDark, setIsDark }) {
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <nav className="navbar">
       <div className="nav-logo">
-        <h3>AdjacencyMatrix </h3>
+        <h3>AdjacencyMatrix</h3>
       </div>
 
-      <ul className="nav-links">
+      {/* Burger Button */}
+      <button
+        className={`burger ${isOpen ? "active" : ""}`}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+
+      {/* Nav Links */}
+      <ul className={`nav-links ${isOpen ? "open" : ""}`}>
         <li><a href="/" className="nav-link active">Home</a></li>
         <li><a href="#" className="nav-link">About</a></li>
         <li><a href="#" className="nav-link">How to Use</a></li>
         <li><a href="#" className="nav-link">Contact</a></li>
-        <button
-          className="theme-toggle"
-          onClick={() => setIsDark(prev => !prev)}
-          aria-label="Toggle theme"
-        >
-          {isDark ? "☀️" : "🌙"}
-        </button>
+        <li>
+          <div className="ThemeButton">
+          <button
+            className="theme-toggle"
+            onClick={() => setIsDark(prev => !prev)}
+            aria-label="Toggle theme"
+          >
+            {isDark ? "☀️" : "🌙"}
+          </button>
+          </div>
+        </li>
       </ul>
-
-      
     </nav>
   );
 }
